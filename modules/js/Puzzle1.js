ddf9014decac2f04a4477337e2d5a5cb49694580
User-PC <masood.mansoori@gmail.com>
2025-02-06 15:57:08 +1100

    1

--- modules/js/Puzzle1.js
+++ modules/js/Puzzle1.js
@@ -1,18 +1,24 @@
 //Checks if the answer given for puzzle 1 is correct
 
-//If the correct multiple choice answer was selected, display the puzzle1-correct text
 function ValidatePuzzle1() {
-  document.getElementById("puzzle1-correct").style.display = "block";
+  //Get the answer the player has given and make it lowercase
+  var playersAnswer = document.getElementById("puzzle1Ans").value.toLowerCase();
+  //If the players answer matches the text we're looking for set the puzzle6-correct texts style.display to block, ie make it show
+  if (playersAnswer.includes("a clock") || playersAnswer.includes("A clock")) || playersAnswer.includes("A Clock")){
+    document.getElementById("puzzle1-correct").style.display = "block";    
+  } else { //If the answer doesnt match, call the Puzzle6Wrong function
+    Puzzle1Wrong();
+  }
 }
 
-//If the wrong answer was selected, display the puzzle1-wrong-ans text
-//Then call the hidePuzzle1 function after 4000ms
+//If the answer given was wrong, the puzzle1-wrong-ans text will display
+//Then we'll call the HidePuzzle1 function after 4000ms to hide the text again
 function Puzzle1Wrong() {
   document.getElementById("puzzle1-wrong-ans").style.display = "block";
-  wrongAnswer = setTimeout(hidePuzzle1, 4000);
+  wrongAnswer = setTimeout(HidePuzzle1, 4000);
 }
 
 //Hide the puzzle1-wrong-ans text
-function hidePuzzle1() {
+function HidePuzzle1() {
   document.getElementById("puzzle1-wrong-ans").style.display = "none";
 }
