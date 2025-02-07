c95a12ad6cfb45ab65e5a51bcacc7bcc4c75c600
User-PC <masood.mansoori@gmail.com>
2025-02-07 15:00:13 +1100

    7

--- modules/js/Collapsible.js
+++ modules/js/Collapsible.js
@@ -1,21 +1,16 @@
 // from w3's collapsible how to: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
 
-//Get the elements with the class name 'collapsible'
-var collapsible = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-//Go through each of the elements we got, one at a time and add a 'click' event listener with the below function
-for (var i = 0; i < collapsible.length; i++) 
-{ 
-  collapsible[i].addEventListener("click", function () 
-  { this.classList.toggle("active");
-    //Get whatever is next in the html and put it in the 'content' variable
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
     var content = this.nextElementSibling;
-    //If whatever is in 'content' has its style.display element set to block
-    if (content.style.display === "block") 
-    {
-      content.style.display = "none"; //Set it to none, ie. make it hidden
+    if (content.style.display === "block") {
+      content.style.display = "none";
     } else {
-      content.style.display = "block"; //If not, set it to block, ie. make is show
+      content.style.display = "block";
     }
   });
 }
